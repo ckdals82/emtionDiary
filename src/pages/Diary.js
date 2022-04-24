8f896bce77956b510cb176fd315e978ba5d3b9af
ckdals82 <ckdals82@naver.com>
2022-04-24 22:17:47 +0900

    다이어리 페이지 작업

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,14 +1,56 @@
-import { useParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryStateContext } from '../App';
+import MyButton from '../components/MyButton';
+import MyHeader from '../components/MyHeader';
+
+import { getStringDate } from '../utill/date';
 
 const Diary = () => {
   const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      console.log(targetDiary);
+
+      if (targetDiary) {
+        //일기가 있을때
+        setData(targetDiary);
+      } else {
+        //일기가 없을때
+        alert('없는 일기입니다.');
+        navigate('/', { replace: true });
+      }
+    }
+  }, [id, diaryList]);
   //diary: id  이렇게 지정해서 id로 써야함
-  return (
-    <div>
-      <h2>diary</h2>
-      <p>이곳은 이곳은 일기 상세 페이지입니다.</p>
-    </div>
-  );
+
+  if (!data) {
+    return <div className='DiaryPage'>로딩중입니다.</div>;
+  } else {
+    return (
+      <div className='DiaryPage'>
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))}의 기록`}
+          leftChild={
+            <MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)} />
+          }
+          rightChild={
+            <MyButton
+              text={'수정하기'}
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+        />
+      </div>
+    );
+  }
 };
 
 export default Diary;
