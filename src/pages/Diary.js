b189d81cdc5e65304a5b785a6d4278482729d711
ckdals82 <ckdals82@naver.com>
2022-04-24 23:27:42 +0900

    상세 페이지 작업 ver2

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -5,6 +5,7 @@ import MyButton from '../components/MyButton';
 import MyHeader from '../components/MyHeader';
 
 import { getStringDate } from '../utill/date';
+import { emotionList } from '../utill/emotion';
 
 const Diary = () => {
   const { id } = useParams();
@@ -13,31 +14,39 @@ const Diary = () => {
   const [data, setData] = useState();
 
   useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `감정 일기장 - ${id}번 일기`;
+  }, []);
+
+  useEffect(() => {
     if (diaryList.length >= 1) {
       const targetDiary = diaryList.find(
         (it) => parseInt(it.id) === parseInt(id)
       );
-      console.log(targetDiary);
 
       if (targetDiary) {
-        //일기가 있을때
+        // 일기가 존재할 때
         setData(targetDiary);
       } else {
-        //일기가 없을때
+        // 일기가 없을 때
         alert('없는 일기입니다.');
         navigate('/', { replace: true });
       }
     }
   }, [id, diaryList]);
-  //diary: id  이렇게 지정해서 id로 써야함
 
   if (!data) {
-    return <div className='DiaryPage'>로딩중입니다.</div>;
+    return <div className='DiaryPage'>로딩중입니다...</div>;
   } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+    console.log(curEmotionData);
+
     return (
       <div className='DiaryPage'>
         <MyHeader
-          headText={`${getStringDate(new Date(data.date))}의 기록`}
+          headText={`${getStringDate(new Date(data.date))} 기록`}
           leftChild={
             <MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)} />
           }
@@ -48,6 +57,28 @@ const Diary = () => {
             />
           }
         />
+        <article>
+          <section>
+            <h4>오늘의 감정</h4>
+            <div
+              className={[
+                'diary_img_wrapper',
+                `diary_img_wrapper_${data.emotion}`
+              ].join(' ')}
+            >
+              <img src={curEmotionData.emotion_img} />
+              <div className='emotion_descript'>
+                {curEmotionData.emotion_descript}
+              </div>
+            </div>
+          </section>
+          <section>
+            <h4>오늘의 일기</h4>
+            <div className='diary_content_wrapper'>
+              <p>{data.content}</p>
+            </div>
+          </section>
+        </article>
       </div>
     );
   }
